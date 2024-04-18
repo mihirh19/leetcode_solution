3801f30fa187e636e5ddbf4ad120fc2ecef240a6
Mihir Hadavani <miheerhadvani990@gmail.com>
2024-04-18 23:54:17 +0530

    Time: 49 ms (10.39%), Space: 16.8 MB (13.64%) - LeetHub

---
+++ 2304-cells-in-a-range-on-an-excel-sheet/2304-cells-in-a-range-on-an-excel-sheet.py
@@ -0,0 +1,10 @@
+class Solution:
+    def cellsInRange(self, s: str) -> List[str]:
+        c1, c2 = ord(s[0]), ord(s[3])
+        r1, r2 = int(s[1]), int(s[4])
+        li = []
+        for c in range(c1,c2+1):
+            for r in range(r1, r2+1):
+                li.append("" + chr(c) + str(r))
+        
+        return li
