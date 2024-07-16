45f27c1548f689a87ffd945b9ea70ff10e551ea9
Mihir Hadavani <miheerhadvani990@gmail.com>
2024-07-16 21:17:37 +0530

    Time: 31 ms (89.19%), Space: 16.4 MB (73.6%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,8 @@
+class Solution:
+    def generate(self, n: int) -> List[List[int]]:
+        ans = [[1] * i for i in range(1, n + 1)]
+        for i in range(1, n):
+            for j in range(1, i):
+                ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j]
+
+        return ans
